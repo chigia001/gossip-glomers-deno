3f8627370e18419c40df734c59d122ede54d43f4
Chi Ma <chigia001@gmail.com>
2023-03-15 13:45:11 +0700

    Update README

--- README.md
+++ README.md
@@ -15,3 +15,17 @@
 ```bash
 ./maelstrom test -w unique-ids --bin ./unique-id.ts --time-limit 30 --rate 1000 --node-count 3 --availability total --nemesis partition
 ```
+
+### Broadcast:
+#### a: Single-Node Broadcast
+```bash
+./maelstrom test -w broadcast --bin ./broadcast.ts --node-count 1 --time-limit 20 --rate 10
+```
+#### b: Multi-Node Broadcast
+```bash
+./maelstrom test -w broadcast --bin ./broadcast.ts --node-count 5 --time-limit 20 --rate 10
+```
+#### c: Fault Tolerant Broadcast
+```bash
+./maelstrom test -w broadcast --bin ./broadcast.ts --node-count 5 --time-limit 20 --rate 10 --nemesis partition
+```
--- broadcast.ts
+++ broadcast.ts
@@ -3,18 +3,17 @@ import { handle, init, NodeId, NodeIds, NodeIdType, rpc, send } from "./Node.ts"
 let topology: Record<NodeIdType, NodeIdType[]>
 const messages = new Set<number>()
 
-const handleBroadcast = async (message: number, ignoreNodes: NodeIdType[] = [])  => {
+const handleBroadcast = async (message: number, ignoreNodes: string = '')  => {
     if (messages.has(message)) {
         return;
     }
 
     messages.add(message)
     const currentNodeId = NodeId()
-    const sendSet = new Set([...ignoreNodes, currentNodeId])
-    const peerSet = new Set((topology[currentNodeId] || []).filter(node => !sendSet.has(node)))
+    const sends = `${ignoreNodes}_${currentNodeId}`
+    const peerSet = new Set((topology[currentNodeId] || []).filter(node => sends.indexOf(`_${node}`) === -1))
     
     const peers = [...peerSet.values()]
-    const sends = [...sendSet.values()]
 
     await Promise.all(
         peers.map((dest) => rpc(dest, {
@@ -40,7 +39,7 @@ handle<
 })
 
 handle<
-    {message: number, sends: NodeIdType[]}
+    {message: number, sends: string}
 >("broadcast_peer", async (_src, req, reply) => {
     await handleBroadcast(req.message, req.sends)
     reply()
